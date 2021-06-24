a1f3840c13d350d7b539a241ba3da644211d65a1
dylan-toomey <dtoomey322@gmail.com>
2021-06-24 18:07:03 +1200

    Initial Commit for setup

---
+++ client/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App () {
+  // useEffect(() => {
+  //   getWelcome()
+  //     .then(res => {
+  //       setWelcomeStatement(res.statement)
+  //       return null
+  //     })
+  //     .catch((err) => {
+  //       console.error(err.message)
+  //     })
+  // })
+
+  return (
+    <h1>Team Get</h1>
+  )
+}
+
+export default App
