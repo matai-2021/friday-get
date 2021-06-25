26ac700cf9a60eb12c7ccb241f764793af70aa07
kritika-anita <82843287+kritika-anita@users.noreply.github.com>
2021-06-25 12:13:58 +1200

    Display movies on the browser

--- client/components/Movies.jsx
+++ client/components/Movies.jsx
@@ -1,65 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getMovies } from '../api.js'
 
 function Movies () {
-  // useEffect(() => {
-  //   getWelcome()
-  //     .then(res => {
-  //       setWelcomeStatement(res.statement)
-  //       return null
-  //     })
-  //     .catch((err) => {
-  //       console.error(err.message)
-  //     })
-  // })
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    getMovies()
+      .then(res => {
+        console.log(res)
+        setMovies(res.results)
+        // console.log(res.results)
+        return null
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  })
 
   return (
     <div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
-      <div>
-        <img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
-        <h2>Luca</h2>
-      </div>
+      { movies.map(item => (
+        <div key={item.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}/>
+          <h2>{item.original_title}</h2>
+        </div>
+      )
+      )}
     </div>
   )
 }
 
 export default Movies
+
+// Backup reference for HTML
+{ /* <div>
+<img src="https://image.tmdb.org/t/p/w500/7rhzEufovmmUqVjcbzMHTBQ2SCG.jpg"></img>
+<h2>Luca</h2>
+</div> */ }
