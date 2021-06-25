0dc423be9ec587abe0ca07cb597b7fd60e30b978
Cam-Ferg <82124521+Cam-Ferg@users.noreply.github.com>
2021-06-25 15:19:39 +1200

    finish MVP

--- client/components/Movies.jsx
+++ client/components/Movies.jsx
@@ -3,19 +3,20 @@ import { Link } from 'react-router-dom'
 
 function Movies ({ movies }) {
   return (
-    <div className="grid grid-cols-4">
-      { movies.map(item => (
-    
-      <Link key={item.id} to={`/movie/${item.id}`}>
-        <div className="m-auto" key={item.id}>
-          <img className="h-96 rounded-md m-auto shadow-md" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}/>
-          <h2 className="font-helvetica text-center pt-2 pb-8 mx-16">{item.original_title}</h2>
-        </div>
-        </Link>
-
-      )
-      )}
-    </div>
+    <>
+      <h1 className="font-helvetica font-bold text-center text-6xl py-8">These are Movies</h1>
+      <div className="grid grid-cols-4">
+        { movies.map(item => (
+          <div className="m-auto" key={item.id}>
+            <Link key={item.id} to={`/movie/${item.id}`}>
+              <img className="h-96 rounded-md m-auto shadow-md" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}/>
+            </Link>
+            <h2 className="font-helvetica text-center pt-2 pb-8 mx-16">{item.original_title}</h2>
+          </div>
+        )
+        )}
+      </div>
+    </>
   )
 }
 export default Movies
